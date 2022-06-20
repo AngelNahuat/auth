9998f91d71b49de065a1592da3cf6b59e32cc236
AngelNahuat <nglnahuat@gmail.com>
2022-06-20 17:48:05 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:auth/auth_controller.dart';
+import 'package:auth/login_page.dart';
+import 'package:auth/signup_page.dart';
+import 'package:auth/splash.dart';
+import 'package:auth/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
